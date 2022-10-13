8d081fa58c424f0662978d994f6b06b81015be94
BrianG1122 <brian.green1122@gmail.com>
2022-10-13 17:01:29 -0400

    Updated the style using DOM

--- script.js
+++ script.js
@@ -8,6 +8,12 @@
 
 // document.querySelector('.guess').value = 23;
 
+// This gives us a number between 1 - 20
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 // Selected a button with class name of 'check' using querySelector
 // Then used the addEventListener method to add a event handler
 document.querySelector('.check').addEventListener('click', function () {
@@ -17,7 +23,38 @@ document.querySelector('.check').addEventListener('click', function () {
   // guess is a falsey value
   // we use the ! operator to convert it to true
   // then the block of code within the if statement is able to run
+  // the else if statements show a message if the guess is either correct, empty, too high or too low
+
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess it too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😢 You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
