f5a3b4d6fb0ff765d185baff5e6b8714dd399ba8
BrianG1122 <brian.green1122@gmail.com>
2022-10-13 15:47:06 -0400

    Added event listener and if statement

--- script.js
+++ script.js
@@ -11,5 +11,13 @@
 // Selected a button with class name of 'check' using querySelector
 // Then used the addEventListener method to add a event handler
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // guess is a falsey value
+  // we use the ! operator to convert it to true
+  // then the block of code within the if statement is able to run
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
 });
